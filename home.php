9aea5ce11cb001894dd4daa2c50d5dc7b4e4be99
ashrafabueraq <ashrafabueraq97@gmail.com>
2022-05-04 15:46:50 +0300

    main

--- home.php
+++ home.php
@@ -1,27 +1,18 @@
-<?php 
-session_start();
-
-
-?>
-
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <title>View Page</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
 
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
   <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
+
+
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
@@ -37,136 +28,111 @@ session_start();
         <li class="nav-item">
           <a class="nav-link" href="add.php">Add Product</a>
         </li>
-        <!-- <li class="nav-item">
-        <form method="post" action="index.php">
-          <input type="submit" name="clear" value="clear" style="margin: 21%;width: 150%;">
-          </form>
-        </li> -->
       </ul>
     </div>
   </div>
 </nav>
-  
-    <div class="container">
+
+<div class="container">
       <div class="row">
-        <!-- <h2>OUR PRODUCTS </h2> -->
+       
         <div class="col-md-4">
           <div class="card" style="width: 18rem;  height: 572px;">
-            <img src="image/phone.jpg" class="card-img-top"
-              alt="...">
+            <img src="image/lab.avif" class="card-img-top"
+              alt="..." height="250px">
             <div class="card-body">
-              <h5 class="card-title">Product Name</h5>
-              <p class="card-text">
-                Description</p>
-                <p class="card-text"> Price $ </p>
-                <button type="submit" class="buy">BUY NOW</button>
-                
+              <h5 class="card-title">LabTop</h5>
+              <p class="card-text"> Your laptop's specifications include its processor, RAM, operating system, and the device and product ID</p>
+               
+                <p class="card-text"> 2500 $ </p>
+                <button type="submit" class="buy">BUY NOW</button> 
             </div>
           </div>
         </div>
 
 
-   
         <?php
-
-if (isset($_SESSION['names'])) {
-  $nt= $_SESSION['names'];
-}
-
-
-if (isset($_SESSION['imgs'])) {
-  $it= $_SESSION['imgs'] ;
-}
- 
-
-if (isset($_SESSION['prices'])) {
-  $pt= $_SESSION['prices'];
-}
-
-
-if (isset($_SESSION['descs'])) {
-  $dt= $_SESSION['descs'];
- }
-       
-
         
+        session_start();
 
 
-// Card......
+        if(isset($_SESSION['product'])){
 
-        if( empty($nt) == false ){
-          for ($i=1; $i <count($nt);++$i ){
-            echo '<div class="col-md-4">';
-            echo '<div class="card" style="width: 18rem;  height: 572px; ">';
-            echo   '<img src="image/phone.jpg" alt=" nothing error ">';
-            echo   '<div class="card-body">';
-            echo     '<h5 class="card-title">'. $nt[$i]. '</h5>';
-            echo     '<p class="card-text">' . $dt[$i].'</p>';
-            echo     '<p class="card-text">'. $pt[$i]. "$".'</p>';
-            echo    '<button type="submit" class="buy">BUY NOW</button>';
-            echo   ' </div>';
-            echo   '</div>';
-            echo  '</div>';
-  
+
+          for($i=0; $i < count($_SESSION['product']); $i++){
+
+            echo ' <div class="col-md-4">';
+            echo '<div class="card" style="width: 18rem;  height: 572px;">';
+            echo '<img src="image/phone.jpg" class="card-img-top" alt="..." height="250px">';
+            echo ' <div class="card-body">';
+            echo '<h5 class="card-title">'.$_SESSION['product'][$i]['name'].'</h5>';
+            echo ' <p class="card-text">'.$_SESSION['product'][$i]['desc'].'</p>';
+            echo '<p class="card-text">'.$_SESSION['product'][$i]['price'].' $'.'</p>';
+            echo '<button type="submit" class="buy">BUY NOW</button>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+
+           
+            
+
           }
-
         }
         
-              
-
-        ?> 
-
-
-
-
+        
+        ?>
+        
+</div>
+</div>
 
 
 
-      </div>
-    </div>
 
-  </main>
+
+
+
+
+
   <!-- Footer -->
   <footer class="bg-dark text-center text-white">
-  <!-- Grid container -->
-  <div class="container p-4">
-    <!-- Section: Social media -->
-    <section class="mb-4">
-      <!-- Facebook -->
-      <a class="btn btn-outline-light btn-floating m-1" href="https://web.facebook.com/salmspehat" role="button"><i class="fab fa-facebook-f"></i></a>
 
-      <!-- Google -->
-      <a class="btn btn-outline-light btn-floating m-1" href="ashrafabueraq97@gmail.com" role="button"><i class="fab fa-google"></i></a>
-
-      <!-- Instagram -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
-
-      <!-- Linkedin -->
-      <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/ashraf-abueraq" role="button"><i class="fab fa-linkedin-in"></i></a>
-
-      <!-- Github -->
-      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/ashrafabueraq" role="button"><i class="fab fa-github"></i></a>
-    </section>
+<div class="container p-4">
  
+  <section class="mb-4">
+    <!-- Facebook -->
+    <a class="btn btn-outline-light btn-floating m-1" href="https://web.facebook.com/salmspehat" role="button"><i class="fab fa-facebook-f"></i></a>
 
+    <!-- Google -->
+    <a class="btn btn-outline-light btn-floating m-1" href="ashrafabueraq97@gmail.com" role="button"><i class="fab fa-google"></i></a>
 
-    <!-- Section: Text -->
-    <section class="mb-4">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-        repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
-        eum harum corrupti dicta, aliquam sequi voluptate quas.
-      </p>
-    </section>
+    <!-- Instagram -->
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
 
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2022 Copyright : by ASHRAF ABUERAQ
-    <a class="text-white" href="pruductpage.php"></a>
-  </div>
+    <!-- Linkedin -->
+    <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/ashraf-abueraq" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+    <!-- Github -->
+    <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/ashrafabueraq" role="button"><i class="fab fa-github"></i></a>
+  </section>
   
-</footer>
 
+
+  
+  <section class="mb-4">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+      repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+      eum harum corrupti dicta, aliquam sequi voluptate quas.
+    </p>
+  </section>
+
+
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  © 2022 Copyright : by ASHRAF ABUERAQ
+  <a class="text-white" href="pruductpage.php"></a>
+</div>
+
+</footer>
 
 
 
@@ -177,8 +143,5 @@ if (isset($_SESSION['descs'])) {
     integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
     crossorigin="anonymous"></script>
 </body>
-
-
-
-
 </html>
+
